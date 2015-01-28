2c61efe4f60d29b0d6dac8c340c50c2ff10b5717
Ryan Abernathey <rpa@ldeo.columbia.edu>
2015-01-28 00:46:57 -0500

    new notebook

--- global_latlon_layers_analysis/helper.py
+++ global_latlon_layers_analysis/helper.py
@@ -11,10 +11,16 @@ def process_layers(ddir, iter0, diters, N, deltaTclock=900, layers_name='1RHO',
     m = MITgcmmodel.ModelInstance(ddir, default_iter=list(iters))
 
 
-    layers_transport = m.rdmds('DiagLAYERS-transport', iters[1:]).mean(axis=0)
+    layers_transport = m.rdmds('DiagLAYERS-transport', iters[1:])
+    layers_diapycnal = m.rdmds('DiagLAYERS-diapycnal', iters[1:])
+    if N > 2:
+        layers_transport = layers_transport.mean(axis=0)
+        layers_diapycnal = layers_diapycnal.mean(axis=0)
+    
+    
     uh, vh = layers_transport[0], layers_transport[3]
+    print 'uh vh shape =', uh.shape, vh.shape
     pi_s = layers_transport[5]
-    layers_diapycnal = m.rdmds('DiagLAYERS-diapycnal', iters[1:]).mean(axis=0)
     layers_trend = m.rdmds('DiagLAYERS-trend', iters)
     
     if layers_trend_Hc_index is not None:
@@ -35,11 +41,14 @@ def process_layers(ddir, iter0, diters, N, deltaTclock=900, layers_name='1RHO',
     
     # "direct" form of advective vel
     advective_vel_direct = la.diapycnal_velocity(layers_diapycnal[np.r_[3,4,8,9]]).sum(axis=0)
+    
+    # "TOTTEND"
+    diapycnal_vel_tottend = -la.diapycnal_velocity(layers_diapycnal[np.r_[10,11]]/(24*60*60)).sum(axis=0)
 
     print dvol_dt.shape, diapycnal_vel.sum(axis=0).shape, advective_vel.shape
     diapycnal_vel_numerical = dvol_dt - diapycnal_vel.sum(axis=0) - advective_vel
     diapycnal_vel_numerical_direct = dvol_dt - diapycnal_vel.sum(axis=0) - advective_vel_direct
-
+    diapycnal_vel_numerical_tottend = dvol_dt - diapycnal_vel_tottend
     
     psi = la.calculate_moc(vh) / 1e6
     print psi.shape
@@ -52,7 +61,9 @@ def process_layers(ddir, iter0, diters, N, deltaTclock=900, layers_name='1RHO',
             'advective_vel': advective_vel,
             'advective_vel_direct': advective_vel_direct,
             'diapycnal_vel_numerical': diapycnal_vel_numerical,
-            'diapycnal_vel_numerical_direct': diapycnal_vel_numerical_direct,        
+            'diapycnal_vel_numerical_direct': diapycnal_vel_numerical_direct,
+            'diapycnal_vel_tottend': diapycnal_vel_tottend,
+            'diapycnal_vel_numerical_tottend': diapycnal_vel_numerical_tottend,            
             'psi': psi, 'pi': pi, 'la': la, 'm': m}
 
 def plot_diapycnal_velocities(d, r=0.7e6, ylim = [28.2, 22], direct=False):
@@ -73,16 +84,18 @@ def plot_diapycnal_velocities(d, r=0.7e6, ylim = [28.2, 22], direct=False):
 
     plt.figure(figsize=(16,12))
 
-    plt.subplot(221)
+    plt.subplot(321)
     plt.pcolormesh(Yg, lb, d['diapycnal_vel'].sum(axis=0).sum(axis=-1), cmap='RdBu_r')
     plt.clim([-r, r])
     plt.colorbar()
+    print 'Yg.shape', Yg.shape
+    print "d['psi'].shape", d['psi'].shape
     plt.contour(Yg[:-1], lbc, d['psi'], 10, colors='k')
     #plt.contour(Yg[:-1], lbc, pi, [0.05, 0.5, 0.95], colors='y')
     plt.ylim(ylim)
     plt.title('Explicit Diapycnal Velocity')
 
-    plt.subplot(222)
+    plt.subplot(322)
     plt.pcolormesh(Yg, lb, d['dvol_dt'].sum(axis=-1), cmap='RdBu_r')
     plt.clim([-r, r])
     plt.colorbar()
@@ -90,7 +103,7 @@ def plot_diapycnal_velocities(d, r=0.7e6, ylim = [28.2, 22], direct=False):
     plt.ylim(ylim)
     plt.title('Volume Trend')
 
-    plt.subplot(223)
+    plt.subplot(323)
     plt.pcolormesh(Yg, lb, d['advective_vel' + diapyc_suff].sum(axis=-1), cmap='RdBu_r')
     plt.clim([-r, r])
     plt.colorbar()
@@ -98,8 +111,24 @@ def plot_diapycnal_velocities(d, r=0.7e6, ylim = [28.2, 22], direct=False):
     plt.ylim(ylim)
     plt.title('Advective Diapycnal Velocity')
 
-    plt.subplot(224)
+    plt.subplot(324)
     plt.pcolormesh(Yg, lb, d['diapycnal_vel_numerical' + diapyc_suff].sum(axis=-1), cmap='RdBu_r')
+    plt.clim([-r, r])
+    plt.colorbar()
+    plt.contour(Yc, lbc, d['psi'], 10, colors='k')
+    plt.ylim(ylim)
+    plt.title('Numerical Diapycnal Velocity')
+    
+    plt.subplot(325)
+    plt.pcolormesh(Yg, lb, d['diapycnal_vel_tottend'].sum(axis=-1), cmap='RdBu_r')
+    plt.clim([-r, r])
+    plt.colorbar()
+    plt.contour(Yc, lbc, d['psi'], 10, colors='k')
+    plt.ylim(ylim)
+    plt.title('TOTTEND Diapycnal Velocity')
+
+    plt.subplot(326)
+    plt.pcolormesh(Yg, lb, d['diapycnal_vel_numerical_tottend'].sum(axis=-1), cmap='RdBu_r')
     plt.clim([-r, r])
     plt.colorbar()
     plt.contour(Yc, lbc, d['psi'], 10, colors='k')
