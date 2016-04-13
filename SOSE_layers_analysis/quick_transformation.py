c863f99e4edc57687c915f9ce3906dc68fca79c9
Ryan Abernathey <rpa@ldeo.columbia.edu>
2016-04-13 08:34:15 -0400

    TS notebook

--- SOSE_layers_analysis/quick_transformation.py
+++ SOSE_layers_analysis/quick_transformation.py
@@ -16,7 +16,7 @@ def surface_transformation_rate(rac, rho1, rho1levs,
     # mask anything outside the range
     rho1_idx, mask1 = get_rho_idx(rho1, rho1levs)
     assert rho1_idx.min()>=0
-    assert rho1_idx.max()<=Nbins1
+    assert rho1_idx.max()<Nbins1
         
     if rho2 is None:
         rho_idx = rho1_idx
@@ -28,19 +28,19 @@ def surface_transformation_rate(rac, rho1, rho1levs,
         drho2 = np.diff(rho2levs)
         Nbins2 = len(rho2levs)-1
         assert rho2_idx.min()>=0
-        assert rho2_idx.max()<=Nbins2
+        assert rho2_idx.max()<Nbins2
         
         # turn 2d coords into 1d indexer
         # rho1_idx is the inner axis
         rho_idx = Nbins1*rho2_idx + rho1_idx
         mask = mask1 | mask2
-        drho = drho1 * drho2
+        drho = np.outer(drho2, drho1).ravel()
         Nbins = Nbins1 * Nbins2
     
     res = []
     for a in fields:
         dens_flux = np.ma.masked_array(a * rac, mask)
-        q = np.bincount(rho_idx, weights=dens_flux.compressed(), minlength=Nbins) / drho
+        q = np.bincount(rho_idx.compressed(), weights=dens_flux.compressed(), minlength=Nbins) / drho
         if rho2 is not None:
             q = q.reshape([Nbins2, Nbins1])
         res.append(q)
@@ -50,8 +50,11 @@ def get_rho_idx(rho, rholevs):
     rho_m = np.ma.masked_greater_equal(
             np.ma.masked_less(rho, rholevs.min()), rholevs.max())
     mask = rho_m.mask
-    rho_idx = np.digitize(rho_m.compressed(), rholevs)-1
-    return rho_idx, mask
+    # don't want to compress because we want to preserve the size of the array
+    #rho_idx = np.digitize(rho_m.compressed(), rholevs)-1
+    Nrho = len(rholevs)
+    rho_idx = np.ma.masked_outside(np.digitize(rho, rholevs)-1, 0, Nrho-2)
+    return rho_idx, rho_idx.mask
 
 def idx_2d_to_1d(rho1_idx, Nbins1, rho2_idx, Nbins2):
     """Convert a pair of indices to a single index"""
